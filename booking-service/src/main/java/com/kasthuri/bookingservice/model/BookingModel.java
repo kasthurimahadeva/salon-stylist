19af509f1336acdcae176a003cb093328056df9e
kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-01-27 13:19:42 +0530

    import booking service files

---
+++ booking-service/src/main/java/com/kasthuri/bookingservice/model/BookingModel.java
@@ -0,0 +1,72 @@
+package com.kasthuri.bookingservice.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class BookingModel {
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String id;
+    private String SalonId;
+    private String stylistId;
+    private Date bookingDate;
+    private String status;
+
+    public BookingModel() {
+    }
+
+    public BookingModel(String salonId, String stylistId, Date bookingDate, String status) {
+        SalonId = salonId;
+        this.stylistId = stylistId;
+        this.bookingDate = bookingDate;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSalonId() {
+        return SalonId;
+    }
+
+    public void setSalonId(String salonId) {
+        SalonId = salonId;
+    }
+
+    public String getStylistId() {
+        return stylistId;
+    }
+
+    public void setStylistId(String stylistId) {
+        this.stylistId = stylistId;
+    }
+
+    public Date getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(Date bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
---
+++ booking-service/src/main/java/com/kasthuri/bookingservice/service/BookingService.java
@@ -0,0 +1,16 @@
+package com.kasthuri.bookingservice.service;
+
+import com.kasthuri.bookingservice.model.BookingModel;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookingService {
+    BookingModel save(BookingModel bookingModel);
+
+    List<BookingModel> getAllBookings();
+
+    Optional<Iterable<BookingModel>> findByStatus(String status);
+
+    Optional<BookingModel> findById(String id);
+}
